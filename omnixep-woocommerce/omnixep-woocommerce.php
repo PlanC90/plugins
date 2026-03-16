042ef9569e02976dbc424c7809a0bf31b08216b7
planc90 <ceyhun.yilmaz@hotmail.com.tr>
2026-03-16 15:56:16 +0300

    Otomatik Guncelleme: Versiyon v2.5.2

--- omnixep-woocommerce/omnixep-woocommerce.php
+++ omnixep-woocommerce/omnixep-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: OmniXEP WooCommerce Payment Gateway
  * Plugin URI: https://www.electraprotocol.com/omnixep/
  * Description: Accept XEP and Tokens via OmniXEP Wallet.
- * Version: 2.4.6
+ * Version: 2.5.2
  * Author: XEPMARKET
  * Author URI: https://xepmarket.com
  * Text Domain: omnixep-woocommerce
